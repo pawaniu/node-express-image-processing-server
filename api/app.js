2ff541fdca3b26b4e87e79cae02824d5610b546f
Pawan Gupta <pawgupta@blueplanet.com>
2022-06-09 12:43:12 +0530

    module2

--- api/app.js
+++ api/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+const router = require('./src/router');
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
 const app = express();
+
+app.use('/', router);
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 app.use('/*', (request, response) => {
     response.sendFile(pathToIndex);
